a023f1a5980de695a0038e6973439457a4d358e9
Vladislav <vladislav@whatasoft.net>
2023-06-14 21:31:15 +0600

    changed url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://visit-checker-back-production.up.railway.app',
+    // baseURL: 'http://localhost:3000',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
